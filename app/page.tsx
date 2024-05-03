7d7a557058d7938019741c16c24789df94ada383
rishi chhetri <rishiapple4@gmail.com>
2024-05-03 15:43:04 +0545

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "STARTUP-NEXTJS_MAIN",
+  description: "This is Home for Startup Nextjs Main",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Contact />
+    </>
+  );
+}
